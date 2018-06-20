2d47a8d1ca4a1b81c9533f9ec6a6fc74c4101a4a
chiabi <chiabi88@gmail.com>
2018-06-20 10:24:43 +0900

    11, 12 key sequence detection

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -31,19 +31,27 @@ function handleRageUpdate() {
 }
 
 function handleProgress() {
-  console.log('boo')
   const percent = (video.currentTime / video.duration) * 100;
-  console.log('foo')
   progressBar.style.flexBasis = `${percent}%`;
-  console.log('bar')
 }
 
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime
+}
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 video.addEventListener('timeupdate', handleProgress);
+
 toggle.addEventListener('click', togglePlay);
 
 skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRageUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRageUpdate));
+
+let mousedown = false;
+progress.addEventListener('click', scrub)
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e))
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = true);
